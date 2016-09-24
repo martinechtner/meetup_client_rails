481478c4221bf392a61c4076ae26e163c38c6365
martinechtner <martin.echtner@gmail.com>
2016-09-24 17:38:52 -0600

    require directly

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,7 +3,9 @@ require 'net/http'
 require 'webmock/rspec'
 
 # Require all files in the project
-Dir[File.expand_path './**/*.rb'].each { |file| require_relative(file) }
+# Dir[File.expand_path 'lib/**/*.rb'].each { |file| require_relative(file) }
+require 'meetup_client_rails'
+
 
 RSpec.configure do |config|
   config.color = true
